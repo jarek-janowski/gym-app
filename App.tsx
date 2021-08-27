12ab45a2d7c34a3ab1830c3faa4df3c712c24012
jarek-janowski <jarjanowski95@gmail.com>
2021-08-27 12:38:47 +0200

    feat: add fonts & first two screens

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
+import { enableScreens } from 'react-native-screens';
+
+enableScreens();
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    'lato-bold': require('./assets/fonts/lato-bold.ttf'),
+    'lato-regular': require('./assets/fonts/lato-regular.ttf'),
+    'lato-light': require('./assets/fonts/lato-light.ttf'),
+  })
+
+  if(!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <View style={styles.container}>
-      <Text>✌</Text>
+      <Text style={styles.text}>LOREM IPSUM</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +34,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontFamily: 'lato-bold'
+  }
 });
---
+++ screens/AddTrainingDayScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native'
+ 
+const AddTrainingDayScreen = () => {
+    return ( 
+        <View>
+            <Text>Training Day Screen</Text>
+        </View>
+     );
+}
+
+const styles = StyleSheet.create({})
+ 
+export default AddTrainingDayScreen;
